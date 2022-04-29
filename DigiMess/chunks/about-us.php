5fae449f4fe239cd7d5ef54850f0649731cda6c3
Pratiksha228 <101017996+Pratiksha228@users.noreply.github.com>
2022-04-29 12:21:35 +0530

    Add files via upload

---
+++ DigiMess/chunks/about-us.php
@@ -0,0 +1,8 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3"> We are Students of MIT Academy of Engineering, Alandi. We are in a third year right now and pursing B.tech in Computer Science and Engineering. We are here to provide you digital canteen/mess system to make your day to day life easier so, that you can eat the quality food and maintained your diet properly.
+				</p>
+		      </div>
+	</section>
---
+++ DigiMess/chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By Our Group</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>The Digimess provides you not only tasty but quality food. We believe in quality.We are master to service you the daily mess food with really good test while maintaining hygiene. We are alway tring to give variety of fodd each other day, because we know that even if you eat your most favourite dish everyday, it would not the same for you!</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
